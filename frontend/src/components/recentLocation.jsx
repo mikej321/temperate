fd3f2c15b9b9933b086f77b707d5fe6da1994a73
michael <michaeljcantley@gmail.com>
2025-09-21 11:31:37 -0400

    update ping

---
+++ frontend/src/components/recentLocation.jsx
@@ -0,0 +1,10 @@
+import '../styles/recentLocations.css';
+
+export default function recentLocations() {
+    return (
+        <div className="recent_locations_container">
+            <div className="recent_locations"></div>
+            <div className="nearby_locations"></div>
+        </div>
+    )
+}
